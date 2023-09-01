d17f2691c148a1f66def0b181b1b1ea013188f3c
MatosOliveira <matosalem.oliveira@gmail.com>
2023-09-01 19:22:58 -0300

    Refatoracao: criacao de funcoes para tratar as operacoes

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -1,58 +1,77 @@
-menu = """
+def menu():
+    menu = """
 
-Digite a opção desejada:
+    Digite a opção desejada:
 
-[d] Depositar
-[s] Sacar
-[e] Extrato
-[q] Sair
+    [d] Depositar
+    [s] Sacar
+    [e] Extrato
+    [q] Sair
 
-=> """
+    => """
 
-saldo = 0.0
-limite = 500.0
-extrato = ""
-deposito = 0
-valor_depositado = 0
-numero_saques = 0
-LIMITE_DIARIO = 3
-valor_sacado = 0
-
-while True:
-    opcao = input(menu) 
-    
-    if opcao == "e":
-        if (numero_saques == 0 and deposito == 0):
-            extrato = "Não foram realizadas movimentações."
-            print(extrato)
-        else:
-            extrato = "Quantidade de saques realizados: {} \nValor sacado: R$ {} \nQuantidade de depositos recebidos: {} \nValor depositado: R$ {} \nSaldo atual: R$ {}"
-            print(extrato.format(numero_saques, valor_sacado, deposito, valor_depositado, saldo))
-
-    elif opcao == "q":
-        break
-
+def sacar(*, saldo, valor):
+    if(numero_saques < LIMITE_DIARIO):
+        extrato += "Quantidade de saques excede o limite diário. Máximo permitido: {LIMITE_DIARIO}"
+    elif(valor <= limite):
+        extrato +=  "Valor excede o limite diário para saque. Valor máximo permitido: {limite}"
     else:
-        valor = float(input("Digite o valor desejado: "))
+        if(saldo > 0):
+            saldo -= valor
+            numero_saques += 1
+            valor_sacado += valor
+            extrato += "Quantidade de saques realizados: {} \nValor sacado: R$ {} \nQuantidade de depositos recebidos: {} \nValor depositado: R$ {} \nSaldo atual: R$ {}"
+        else:
+            print("Saldo insuficiente!")
+
+
+def exibir_extrato():
+    print("testes")
+
+def depositar():
+    print("testes")
+
+def encerrar():
+    print("testes")
+
+def main():
+    saldo = 0.0
+    limite = 500.0
+    extrato = ""
+    deposito = 0
+    valor_depositado = 0
+    numero_saques = 0
+    LIMITE_DIARIO = 3
+    valor_sacado = 0
+
+    while True:
+        opcao = input(menu) 
         
-        if valor > 0:
+        if opcao == "e":
+            if (numero_saques == 0 and deposito == 0):
+                extrato = "Não foram realizadas movimentações."
+                print(extrato)
+            else:
+                extrato = "Quantidade de saques realizados: {} \nValor sacado: R$ {} \nQuantidade de depositos recebidos: {} \nValor depositado: R$ {} \nSaldo atual: R$ {}"
+                print(extrato.format(numero_saques, valor_sacado, deposito, valor_depositado, saldo))
 
-            if opcao == "d":
-                saldo += valor
-                deposito += 1
-                valor_depositado += valor
-
-            elif opcao == "s":
-                if (numero_saques < LIMITE_DIARIO) and (valor <= 500.0):
-                    if(saldo > 0):
-                        saldo -= valor
-                        numero_saques += 1
-                        valor_sacado += valor
-                    else:
-                        print("Saldo insuficiente.")
-                        
-                else:
-                    print("Limite ou valor diario excedido")
+        elif opcao == "q":
+            break
 
         else:
-            print("O valor desejado deve ser maior que 0!")
+            valor = float(input("Digite o valor desejado: "))
+            
+            if valor > 0:
+
+                if opcao == "d":
+                    saldo += valor
+                    deposito += 1
+                    valor_depositado += valor
+
+                elif opcao == "s":
+                    sacar(saldo, valor)
+
+            else:
+                print("O valor desejado deve ser maior que 0!")
+
+main()
